125a908b90f336c77882c3c587a8a98dc556b690
Alan J <S00168420@mail.itsligo.ie>
2017-03-06 11:41:36 +0000

    Hardware Monitor used for the IoT project. Based on hackster.io project

---
+++ ArduinoHardwareMonitor/ArduinoHardwareMonitor/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ArduinoHardwareMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.intrvlBox = new System.Windows.Forms.ComboBox();
+            this.portlbl = new System.Windows.Forms.Label();
+            this.intrvllbl = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.disBtn = new System.Windows.Forms.Button();
+            this.conBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStatusLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.emailLbl = new System.Windows.Forms.Label();
+            this.emailCheckBtn = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // portBox
+            // 
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(12, 32);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(121, 21);
+            this.portBox.TabIndex = 0;
+            // 
+            // intrvlBox
+            // 
+            this.intrvlBox.FormattingEnabled = true;
+            this.intrvlBox.Location = new System.Drawing.Point(12, 97);
+            this.intrvlBox.Name = "intrvlBox";
+            this.intrvlBox.Size = new System.Drawing.Size(121, 21);
+            this.intrvlBox.TabIndex = 1;
+            // 
+            // portlbl
+            // 
+            this.portlbl.AutoSize = true;
+            this.portlbl.Location = new System.Drawing.Point(16, 16);
+            this.portlbl.Name = "portlbl";
+            this.portlbl.Size = new System.Drawing.Size(63, 13);
+            this.portlbl.TabIndex = 2;
+            this.portlbl.Text = "Set the Port";
+            // 
+            // intrvllbl
+            // 
+            this.intrvllbl.AutoSize = true;
+            this.intrvllbl.Location = new System.Drawing.Point(12, 81);
+            this.intrvllbl.Name = "intrvllbl";
+            this.intrvllbl.Size = new System.Drawing.Size(42, 13);
+            this.intrvllbl.TabIndex = 3;
+            this.intrvllbl.Text = "Interval";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(261, 32);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(52, 13);
+            this.status.TabIndex = 4;
+            this.status.Text = "Waiting...";
+            // 
+            // disBtn
+            // 
+            this.disBtn.Location = new System.Drawing.Point(294, 81);
+            this.disBtn.Name = "disBtn";
+            this.disBtn.Size = new System.Drawing.Size(75, 23);
+            this.disBtn.TabIndex = 5;
+            this.disBtn.Text = "Disconnect";
+            this.disBtn.UseVisualStyleBackColor = true;
+            this.disBtn.Click += new System.EventHandler(this.disBtn_Click);
+            // 
+            // conBtn
+            // 
+            this.conBtn.Location = new System.Drawing.Point(187, 81);
+            this.conBtn.Name = "conBtn";
+            this.conBtn.Size = new System.Drawing.Size(75, 23);
+            this.conBtn.TabIndex = 6;
+            this.conBtn.Text = "Connect";
+            this.conBtn.UseVisualStyleBackColor = true;
+            this.conBtn.Click += new System.EventHandler(this.conBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStatusLbl});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 170);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(393, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStatusLbl
+            // 
+            this.toolStatusLbl.Name = "toolStatusLbl";
+            this.toolStatusLbl.Size = new System.Drawing.Size(115, 17);
+            this.toolStatusLbl.Text = "Waiting For Details...";
+            // 
+            // emailBox
+            // 
+            this.emailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.emailBox.Location = new System.Drawing.Point(12, 147);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(262, 20);
+            this.emailBox.TabIndex = 8;
+            this.emailBox.Text = "Enter Email Here...";
+            this.emailBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.emailBox_MouseClick);
+            // 
+            // emailLbl
+            // 
+            this.emailLbl.AutoSize = true;
+            this.emailLbl.Location = new System.Drawing.Point(12, 131);
+            this.emailLbl.Name = "emailLbl";
+            this.emailLbl.Size = new System.Drawing.Size(35, 13);
+            this.emailLbl.TabIndex = 9;
+            this.emailLbl.Text = "Email:";
+            // 
+            // emailCheckBtn
+            // 
+            this.emailCheckBtn.Location = new System.Drawing.Point(294, 145);
+            this.emailCheckBtn.Name = "emailCheckBtn";
+            this.emailCheckBtn.Size = new System.Drawing.Size(75, 23);
+            this.emailCheckBtn.TabIndex = 10;
+            this.emailCheckBtn.Text = "Email Check";
+            this.emailCheckBtn.UseVisualStyleBackColor = true;
+            this.emailCheckBtn.Click += new System.EventHandler(this.emailCheckBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 192);
+            this.Controls.Add(this.emailCheckBtn);
+            this.Controls.Add(this.emailLbl);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.conBtn);
+            this.Controls.Add(this.disBtn);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.intrvllbl);
+            this.Controls.Add(this.portlbl);
+            this.Controls.Add(this.intrvlBox);
+            this.Controls.Add(this.portBox);
+            this.Name = "Form1";
+            this.Text = "Arduino Read";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox portBox;
+        private System.Windows.Forms.ComboBox intrvlBox;
+        private System.Windows.Forms.Label portlbl;
+        private System.Windows.Forms.Label intrvllbl;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Button disBtn;
+        private System.Windows.Forms.Button conBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStatusLbl;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label emailLbl;
+        private System.Windows.Forms.Button emailCheckBtn;
+    }
+}
+
---
+++ ArduinoHardwareMonitor/ArduinoHardwareMonitor/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using OpenHardwareMonitor.Hardware;
+using Temboo.Core;
+using Temboo.Library.Google.Gmail;
+
+namespace ArduinoHardwareMonitor
+{
+    public partial class Form1 : Form
+    {
+        /*
+         *  Class variables used in all methods.
+         */
+         //Getting access to the users computer by turning it into an object
+        Computer userPc = new Computer()
+        {
+            CPUEnabled = true,
+            RAMEnabled = true
+        };
+
+        //Necessary vars used throughout the progamme
+        float cpuTemp, ramClock;
+        private SerialPort arduinoPort = new SerialPort();
+        string ram;
+        DateTime oldTime = DateTime.UtcNow;
+        DateTime currentTime;
+        int messagesSent = 0;
+
+        /*
+         *  These methods are used to initialise the programme.
+         *  When the programme starts, all the elements are placed onto a windows form.
+         *  Then all the active parts are initialised and added to a combo box.
+         *  Then the intervals are added to the interval box.
+         */
+        public Form1()
+        {
+            InitializeComponent();
+            SetInterval();
+            PortInit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            userPc.Open();
+        }
+
+        private void PortInit()
+        {
+            try
+            {
+                arduinoPort.Parity = Parity.None;
+                arduinoPort.DataBits = 8;
+                arduinoPort.StopBits = StopBits.One;
+                arduinoPort.Handshake = Handshake.None;
+                arduinoPort.RtsEnable = true;
+                arduinoPort.DtrEnable = true;
+
+                portBox.Items.Clear();
+                string[] pcPorts = SerialPort.GetPortNames();
+                foreach (var ports in pcPorts)
+                {
+                    portBox.Items.Add(ports);
+                }
+
+                arduinoPort.BaudRate = 9600;
+            }
+            catch(Exception portInitError)
+            {
+                MessageBox.Show(portInitError.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SetInterval()
+        {
+            for (int i = 100; i <= 1000; i += 100)
+            {
+                intrvlBox.Items.Add(i);
+            }
+        }
+
+        /*
+         *  The buttons here are all given a seperate method.
+         *  When the connecect button is clicked, the port is opened and the timer is enabled to begin updates.
+         *  When the disconnect button is clicked, then a DIS message is sent through the serial con.
+         *  The email check button checks for the @ symbol in the email.
+         */
+        private void conBtn_Click(object sender, EventArgs e)
+        {
+            if (portBox.Text == "" || intrvlBox.Text == "" || !emailBox.Text.Contains("@"))
+            {
+                MessageBox.Show("Make sure no fields are empty and try again!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+                    if (!arduinoPort.IsOpen)
+                    {
+                        arduinoPort.PortName = portBox.Text;
+                        arduinoPort.Open();
+                        timer1.Enabled = true;
+                        timer1.Interval = Convert.ToInt32(intrvlBox.Text);
+                        status.Text = "Connected";
+                        toolStatusLbl.Text = "Sending Data...";
+                    }
+                }
+                catch (Exception portOpenErr)
+                {
+                    MessageBox.Show(portOpenErr.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void disBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                arduinoPort.Write("DIS*");
+                arduinoPort.Close();
+            }
+            catch (Exception portCloseErr)
+            {
+                MessageBox.Show(portCloseErr.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            status.Text = "Disconnected";
+            timer1.Enabled = false;
+            toolStatusLbl.Text = "Connect to Arduino...";
+        }
+
+        private void emailCheckBtn_Click(object sender, EventArgs e)
+        {
+            if (emailBox.Text.Contains('@'))
+            {
+                MessageBox.Show("Email is Good!", "Email", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (emailBox.Text == "")
+            {
+                MessageBox.Show("Please enter a proper email address!", "Email Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a proper email address!", "Email Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //When the email box is clicked, the text dissapears
+        private void emailBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            emailBox.Text = "";
+        }
+
+        /*
+         *  Once the connect button is clicked, the timer1_tick method is deployed which works as a trigger for constamt upddates.
+         *  When it is time to update, the GetStatus method is deployed that gets the current temps and ram speed.
+         *  It is then sent to be printed onto the arduino screen.
+         */
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetStatus();
+        }
+
+        private void GetStatus()
+        {
+            foreach (var hardware in userPc.Hardware)
+            {
+                if (hardware.HardwareType == HardwareType.CPU)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            cpuTemp = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+                if (hardware.HardwareType == HardwareType.RAM)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Load)
+                        {
+                            ramClock = sensor.Value.GetValueOrDefault();
+                            ram = String.Format("{0:00.0}", ramClock);
+                        }
+                    }
+                }
+                arduinoPort.Write(cpuTemp + "*" + ram + "@");
+                currentTime = DateTime.UtcNow;
+                if (cpuTemp > 60 && (messagesSent == 0 || currentTime.Subtract(oldTime) > TimeSpan.FromMinutes(3)))
+                    SendEmail(emailBox.Text);
+            }
+        }
+
+        /*
+         *  If the temperature is above 60 degrees an email is sent using the Temboo and Gmail service.
+         */
+        private void SendEmail(string email)
+        { 
+            TembooSession newSession = new TembooSession("alanj1998", "GmailSender", "lTcbyjr8RYAf7MhPKnTWzspL3Lucpu2C");
+            SendEmail sendEmailTemboo = new SendEmail(newSession);
+
+            sendEmailTemboo.setFromAddress("\"Hardware Monitors\" <alan.j2304@gmail.com>");
+            sendEmailTemboo.setUsername("alan.j2304@gmail.com");
+            sendEmailTemboo.setToAddress(email);
+            sendEmailTemboo.setSubject("CPU Overheat!");
+            sendEmailTemboo.setMessageBody("Dear User, \nYour CPU is overheating! Quickly check your computer!\n\nYours Faithfully,\nHardware Monitor Team");
+            sendEmailTemboo.setPassword("qvzyhmjqzwmtdzuf");
+
+            SendEmailResultSet results = sendEmailTemboo.execute();
+            messagesSent++;
+            oldTime = DateTime.UtcNow;
+        }
+    }
+}
